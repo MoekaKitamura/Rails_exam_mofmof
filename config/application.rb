ff6b192b6a3cc61901bec3c6f1cf3983c6d3e87d
moeka kitamura <moeka.k76@gmail.com>
2021-08-19 23:02:53 +0900

    create mvc

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,13 @@ module DevExam
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    
+    config.generators do |g|
+      # この二行の記述で自動生成しない設定を作成しています。
+      g.assets false
+      g.helper false
+    end
   end
 end
